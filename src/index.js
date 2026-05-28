7042ba1928969b65d15932a66c19bdb72d957923
Nova <darkpumpkin.store@gmail.com>
2026-05-28 22:35:52 +0500

    fix: fix typo in user name prompt capitalization

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ console.log("Welcome to the Brain Games!");
 
 
 
-const name = readlineSync.question("May i have your name? ");
+const name = readlineSync.question("May I have your name? ");
 console.log(`Hello, ${name}!`);
 
 if(!gameDescription){
